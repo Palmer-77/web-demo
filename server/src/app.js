a05aa0d0f95124e101b6127a5263695d6e4d9e0d
steveblack <porndittha77@gmail.com>
2020-07-13 12:08:43 +0700

    folder perpared

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,15 @@
 let express = require('express')
 let bodyParser = require('body-parser')
+const {sequelize} = require('./models')
+
+const config = require('./config/config')
+
 const app = express()
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
+
+require('./routes')(app)
 
 app.get('/status',function (req, res){
         res.send('hello nodejs servser')
@@ -23,19 +29,21 @@ app.get('/users', function (req, res) { res.send('เรียกข้อมู
 })
 
 // create user
-app.post('/user/', function (req, res) { res.send('ทาํ การสร้างผใู้ ชง้ าน:' + JSON.stringify(req.body))
+app.post('/user/', function (req, res) { res.send('ทำการสร้างผู้ใช้งาน:' + JSON.stringify(req.body))
 })
 // edit user
-app.put('/user/:userId', function (req, res) { res.send('ทาํ การแกไ้ ขผใุ้ ชง้ าน:' + req.params.userId + ' : ' +
+app.put('/user/:userId', function (req, res) { res.send('ทำการแก้ไขผู้ใช้งาน:' + req.params.userId + ' : ' +
 JSON.stringify(req.body)) })
 // delete user
-app.delete('/user/:userId', function (req, res) { res.send('ทาํ การลบผใุ้ ชง้ าน:' + req.params.userId + ' : ' +
+app.delete('/user/:userId', function (req, res) { res.send('ทำการลบผู้ใช้งาน:' + req.params.userId + ' : ' +
 JSON.stringify(req.body))
 })
 
-let port = 8081
+let port = process.env.PORT || config.port
 
-app.listen(port, function(){
-        console.log(('server running on '+ port))
+sequelize.sync({force: false}).then(() => { 
+        app.listen(port, function () {
+        console.log('Server running on ' + port) 
+        })
 })
 
